cee53dadbecff7f6f22ec1555e4385a39417790a
faz-7 <fasa.pub@gmail.com>
2023-02-09 18:38:49 +0330

    update Customer model that inherit AbstractBaseUser

---
+++ core/customers/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
+
+
+class Customer(AbstractBaseUser, PermissionsMixin):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    image = models.ImageField(upload_to='customers/')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
